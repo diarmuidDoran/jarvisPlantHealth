9b0ac941d7c9be8e321afabc5afd9e52f58adfd0
diarmuidDoran <ddoran08@qub.ac.uk>
2022-09-12 15:37:52 +0100

    added crontab resource link

--- plantweb/src/pages/add-sensor/add-sensor.tsx
+++ plantweb/src/pages/add-sensor/add-sensor.tsx
@@ -1,11 +1,6 @@
 /* istanbul ignore file */
 import React, { memo } from "react";
-import {
-  Button,
-  TextField,
-  Stack,
-  Grid,
-} from "@mui/material";
+import { Button, TextField, Stack, Grid } from "@mui/material";
 import { useAddSensorLogic } from "./use-add-sensor-logic";
 import { useAppStyles } from "use-app-styles";
 //import { useSensorsLogic } from "pages/sensors/use-sensors-logic";
@@ -25,7 +20,7 @@ export const AddSensor = memo(() => {
 
   // useEffect(() => {
   //   onGetSensorData();
-  // }, 
+  // },
   // // eslint-disable-next-line react-hooks/exhaustive-deps
   // []);
 
@@ -33,14 +28,14 @@ export const AddSensor = memo(() => {
 
   return (
     <Grid
-    container
-    direction="row"
-    justifyContent="center"
-    alignItems="center"
-    xs={12}
-  >   
+      container
+      direction="row"
+      justifyContent="center"
+      alignItems="center"
+      xs={12}
+    >
       <Grid xs={12}>
-      <h3 className={classes.page_title}>Add sensor</h3>
+        <h3 className={classes.page_title}>Add sensor</h3>
       </Grid>
       <Grid
         container
@@ -49,44 +44,64 @@ export const AddSensor = memo(() => {
         alignItems="center"
         xs={12}
       >
-      <Grid xs={12}>
-        <p>
-          Complete the below fields to add your new sensor, avoid using existing
-          room names.
-        </p>
-      </Grid>
-      <Grid xs={12}>
-        <TextField
-          id="sensor-name"
-          label="Sensor Name"
-          variant="outlined"
-          value={sensorName}
-          onChange = {handleSensorNameChange}
-          style={{ width: "75%", marginBottom: 10 }}
-        />
-      </Grid>
-      <Grid xs={12}>
-        <TextField
-          id="sensor-call-frequency"
-          label="Sensor Call Frequency"
-          variant="outlined"
-          value={sensorCallFrequency}
-          onChange = {handleSensorCallFrequencyChange}
-          style={{ width: "75%", marginBottom: 10 }}
-        />
-      </Grid>
-      <Grid xs={12}>
-        <TextField
-          id="sensor-connection-pin"
-          label="Sensor Connection Pin (0-47)"
-          variant="outlined"
-          inputProps={{ inputMode: "numeric", pattern: "[0-9]*", min:0, max:47 }}
-          type="number"
-          value={sensorConnectionPin}
-          onChange = {handleSensorConnectionPinChange}
-          style={{ width: "75%", marginBottom: 10 }}
-        />
-      </Grid>
+        <Grid xs={12}>
+          <p>
+            Complete the below fields to add your new sensor, avoid using
+            existing room names.
+          </p>
+        </Grid>
+        <Grid xs={12}>
+          <TextField
+            id="sensor-name"
+            label="Sensor Name"
+            variant="outlined"
+            value={sensorName}
+            onChange={handleSensorNameChange}
+            style={{ width: "75%", marginBottom: 10 }}
+          />
+        </Grid>
+        <Grid xs={12}>
+          <p style={{ marginBottom: 10, whiteSpace: "pre-wrap" }}>
+            Sensor Call frequency is required to be input in a cronTab format.
+            Follow the{" "}
+            <a
+              href="https://crontab.guru/#*/5_*_*_*_*"
+              target="_blank"
+              rel="noreferrer"
+            >
+              crontab guru
+            </a>{" "}
+            link to set your call frequency times and paste them into the Sensor
+            Call frequency text box below.
+          </p>
+        </Grid>
+        <Grid xs={12}>
+          <TextField
+            id="sensor-call-frequency"
+            label="Sensor Call Frequency e.g. */5 * * * * = every 5th minute"
+            variant="outlined"
+            value={sensorCallFrequency}
+            onChange={handleSensorCallFrequencyChange}
+            style={{ width: "75%", marginBottom: 10 }}
+          />
+        </Grid>
+        <Grid xs={12}>
+          <TextField
+            id="sensor-connection-pin"
+            label="Sensor Connection Pin Number(0-47)"
+            variant="outlined"
+            inputProps={{
+              inputMode: "numeric",
+              pattern: "[0-9]*",
+              min: 0,
+              max: 47,
+            }}
+            type="number"
+            value={sensorConnectionPin}
+            onChange={handleSensorConnectionPinChange}
+            style={{ width: "75%", marginBottom: 10 }}
+          />
+        </Grid>
       </Grid>
       <Grid
         container
@@ -95,11 +110,17 @@ export const AddSensor = memo(() => {
         direction="row"
         justifyContent="flex-end"
       >
-      <Grid xs={2}>
-        <Stack spacing={2} direction="row" style={{ width: "75%", marginBottom: 20, marginTop:20} }>
-          <Button variant="outlined" onClick={onSubmit}>Add Sensor</Button>
-        </Stack>
-      </Grid>
+        <Grid xs={2}>
+          <Stack
+            spacing={2}
+            direction="row"
+            style={{ width: "75%", marginBottom: 20, marginTop: 20 }}
+          >
+            <Button variant="outlined" onClick={onSubmit}>
+              Add Sensor
+            </Button>
+          </Stack>
+        </Grid>
       </Grid>
     </Grid>
   );
