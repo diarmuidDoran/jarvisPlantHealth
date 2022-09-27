eb09bfdfb199cced3dc198bcd4d11385c5cb9a52
diarmuidDoran <ddoran08@qub.ac.uk>
2022-09-27 17:36:33 +0100

    user pages commented out as not yet completed

--- plantweb/src/pages/user_account/index.ts
+++ plantweb/src/pages/user_account/index.ts
@@ -1,1 +1,1 @@
-export * from './user_account';
+// export * from './user_account';
--- plantweb/src/pages/user_account/use-user-account-logic.ts
+++ plantweb/src/pages/user_account/use-user-account-logic.ts
@@ -1,76 +1,76 @@
-// import { SelectChangeEvent } from '@mui/material';
-import { ChangeEvent, useCallback, useState } from "react";
-import { useHistory } from "react-router-dom";
-import { useSensors } from "shared/hooks/use-sensors";
-import { PATHS } from "shared/constants";
-import { editUserAccount } from "api/user-account-api/user-account-api";
-import { useUserAccounts } from "shared/hooks/use-user-accounts";
+// // import { SelectChangeEvent } from '@mui/material';
+// import { ChangeEvent, useCallback, useState } from "react";
+// import { useHistory } from "react-router-dom";
+// import { useSensors } from "shared/hooks/use-sensors";
+// import { PATHS } from "shared/constants";
+// import { editUserAccount } from "api/user-account-api/user-account-api";
+// import { useUserAccounts } from "shared/hooks/use-user-accounts";
 
-export const useUserAccountLogic = (id: number) => {
-    const {
-        getUserAccount,
-        editUserAccount,
-        deleteUserAccount,
-        getUserAccountPlants,
-      } = useUserAccounts();
-  const [name, setName] = useState("");
-  const [userName, setUserName] = useState("");
-  const [userEmail, setUserEmail] = useState("");
+// export const useUserAccountLogic = (id: number) => {
+//     const {
+//         getUserAccount,
+//         editUserAccount,
+//         deleteUserAccount,
+//         getUserAccountPlants,
+//       } = useUserAccounts();
+//   const [name, setName] = useState("");
+//   const [userName, setUserName] = useState("");
+//   const [userEmail, setUserEmail] = useState("");
 
-  const { postSensor } = useSensors();
+//   const { postSensor } = useSensors();
 
-  const history = useHistory();
+//   const history = useHistory();
 
-  const handleNameChange = useCallback(
-    ({ target: { value } }: ChangeEvent<HTMLInputElement>) => {
-      setName(value);
-    },
-    [setName]
-  );
+//   const handleNameChange = useCallback(
+//     ({ target: { value } }: ChangeEvent<HTMLInputElement>) => {
+//       setName(value);
+//     },
+//     [setName]
+//   );
 
-  const handleUserNameChange = useCallback(
-    ({ target: { value } }: ChangeEvent<HTMLInputElement>) => {
-      setUserName(value);
-    },
-    [setUserName]
-  );
-  const handleUserEmailChange = useCallback(
-    ({ target: { value } }: ChangeEvent<HTMLInputElement>) => {
-      setUserEmail(value);
-    },
-    [setUserEmail]
-  );
-  // const handleSensorCallFrequencyChange = (event: SelectChangeEvent) => {
-  //     setSensorCallFrequency(event.target.value as string)
-  // };
+//   const handleUserNameChange = useCallback(
+//     ({ target: { value } }: ChangeEvent<HTMLInputElement>) => {
+//       setUserName(value);
+//     },
+//     [setUserName]
+//   );
+//   const handleUserEmailChange = useCallback(
+//     ({ target: { value } }: ChangeEvent<HTMLInputElement>) => {
+//       setUserEmail(value);
+//     },
+//     [setUserEmail]
+//   );
+//   // const handleSensorCallFrequencyChange = (event: SelectChangeEvent) => {
+//   //     setSensorCallFrequency(event.target.value as string)
+//   // };
 
-  const onSubmit = useCallback(async () => {
-    const newSensor = await editUserAccount(
-        id,
-        userName,
-        name,
-        name,
-        userEmail,
-        password,
-    );
-    if (newSensor) {
-      history.push(`${PATHS.sensors}/${newSensor.id}`);
-    }
-  }, [
-    postSensor,
-    history,
-    sensorName,
-    sensorCallFrequency,
-    sensorConnectionPin,
-  ]);
+//   const onSubmit = useCallback(async () => {
+//     const newSensor = await editUserAccount(
+//         id,
+//         userName,
+//         name,
+//         name,
+//         userEmail,
+//         password,
+//     );
+//     if (newSensor) {
+//       history.push(`${PATHS.sensors}/${newSensor.id}`);
+//     }
+//   }, [
+//     postSensor,
+//     history,
+//     sensorName,
+//     sensorCallFrequency,
+//     sensorConnectionPin,
+//   ]);
 
-  return {
-    sensorName,
-    sensorCallFrequency,
-    sensorConnectionPin,
-    handleSensorNameChange,
-    handleSensorCallFrequencyChange,
-    handleSensorConnectionPinChange,
-    onSubmit,
-  };
-};
+//   return {
+//     sensorName,
+//     sensorCallFrequency,
+//     sensorConnectionPin,
+//     handleSensorNameChange,
+//     handleSensorCallFrequencyChange,
+//     handleSensorConnectionPinChange,
+//     onSubmit,
+//   };
+// };
